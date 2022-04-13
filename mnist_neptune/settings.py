19d289d1edbd6e374383c6d25ddf00cc4eefdbd9
Freddie Ruxton <ruxton.f@gmail.com>
2022-04-13 07:45:43 +0100

    feat: make Settings.NEPTUNE_API_TOKEN a SecretStr

--- mnist_neptune/settings.py
+++ mnist_neptune/settings.py
@@ -1,9 +1,9 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, SecretStr
 
 
 class Settings(BaseSettings):
     NEPTUNE_PROJECT_NAME: str
-    NEPTUNE_API_TOKEN: str
+    NEPTUNE_API_TOKEN: SecretStr
 
     class Config:
         env_file = ".env"
